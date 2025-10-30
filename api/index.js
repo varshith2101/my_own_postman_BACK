651eb32dfc19e5587992f69dbae0eb5cfa5c3779
varshith2101 <varshith.mitmpl2022@learner.manipal.edu>
2025-10-30 19:01:33 +0530

    nothing much

--- api/index.js
+++ api/index.js
@@ -11,9 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use ("/api", router);
-// ✅ Replace with your own
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://varsdevonline_db_user:JPsK3NjEFFjaMNCk@postman.pspwc1w.mongodb.net/?appName=postman";
-
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
